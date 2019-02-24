acaee2adb0ca2b4da4b755b8ee8de4222ee5ff59
Arjun Menon <amenon@wisilica.com>
2019-02-24 13:27:29 +0100

    Updates (Saswath)

--- js/modal.js
+++ js/modal.js
@@ -1,22 +1,91 @@
-var elements = $('.modal-overlay, .modal');
+var elements = $('#modal-overlay, #modal');
 
 $('.link1').click(function(){
     elements.addClass('active');
 });
 
-$('.close-modal').click(function(){
+$('#modal-overlay').click(function(){
     elements.removeClass('active');
 });
 
 
-
-
-var elements1 = $('.infant-model-overlay, .infant-modal');
+var elements1 = $('#modal-overlay1, #modal1');
 
 $('#link2').click(function(){
     elements1.addClass('active');
 });
 
-$('.infant-close-modal').click(function(){
+$('#modal-overlay1').click(function(){
     elements1.removeClass('active');
+});
+
+
+var elements3 = $('#modal-overlay3, #modal3');
+
+$('#link3').click(function(){
+    elements3.addClass('active');
+});
+
+$('#modal-overlay3').click(function(){
+    elements3.removeClass('active');
+});
+
+
+
+
+var elements4 = $('#modal-overlay4, #modal4');
+
+$('#link4').click(function(){
+    elements4.addClass('active');
+});
+
+$('#modal-overlay4').click(function(){
+    elements4.removeClass('active');
+});
+
+
+
+var elements5 = $('#modal-overlay5, #modal5');
+
+$('#link5').click(function(){
+    elements5.addClass('active');
+});
+
+$('#modal-overlay5').click(function(){
+    elements5.removeClass('active');
+});
+
+
+
+var elements6 = $('#modal-overlay6, #modal6');
+
+$('#link6').click(function(){
+    elements6.addClass('active');
+});
+
+$('#modal-overlay6').click(function(){
+    elements6.removeClass('active');
+});
+
+
+var elements7 = $('#modal-overlay7, #modal7');
+
+$('#link7').click(function(){
+    elements7.addClass('active');
+});
+
+$('#modal-overlay7').click(function(){
+    elements7.removeClass('active');
+});
+
+
+
+var elements8 = $('#modal-overlay8, #modal8');
+
+$('#link8').click(function(){
+    elements8.addClass('active');
+});
+
+$('#modal-overlay8').click(function(){
+    elements8.removeClass('active');
 });
